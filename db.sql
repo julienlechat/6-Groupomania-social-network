3add7c591b5aed14170b940cb433ec9aa261c87f
Julien LECHAT <3fight@free.fr>
2021-01-22 18:14:56 +0100

    Ajout de la base de donnée

---
+++ db.sql
@@ -0,0 +1,149 @@
+-- MySQL dump 10.13  Distrib 8.0.22, for Win64 (x86_64)
+--
+-- Host: localhost    Database: groupomania
+-- ------------------------------------------------------
+-- Server version	8.0.22
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!50503 SET NAMES utf8mb4 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `post`
+--
+
+DROP TABLE IF EXISTS `post`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `post` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `user` int DEFAULT NULL,
+  `date` datetime DEFAULT NULL,
+  `img` varchar(255) DEFAULT NULL,
+  `text` varchar(255) DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  KEY `PostUser` (`user`),
+  CONSTRAINT `PostUser` FOREIGN KEY (`user`) REFERENCES `users` (`id`) ON DELETE CASCADE ON UPDATE CASCADE
+) ENGINE=InnoDB AUTO_INCREMENT=83 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `post`
+--
+
+LOCK TABLES `post` WRITE;
+/*!40000 ALTER TABLE `post` DISABLE KEYS */;
+INSERT INTO `post` VALUES (80,1,'2021-01-20 07:51:51',NULL,'Bonjour à tous, bienvenue sur groupomania !'),(81,2,'2021-01-20 07:56:50',NULL,'Salut tout le monde !');
+/*!40000 ALTER TABLE `post` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `post_comment`
+--
+
+DROP TABLE IF EXISTS `post_comment`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `post_comment` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `user` int DEFAULT NULL,
+  `msg` varchar(255) DEFAULT NULL,
+  `date` datetime DEFAULT NULL,
+  `id_post` int DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  KEY `id_post` (`id_post`),
+  KEY `PostcommentUser` (`user`),
+  CONSTRAINT `post_comment_ibfk_1` FOREIGN KEY (`id_post`) REFERENCES `post` (`id`) ON DELETE CASCADE,
+  CONSTRAINT `PostcommentUser` FOREIGN KEY (`user`) REFERENCES `users` (`id`) ON DELETE CASCADE ON UPDATE CASCADE
+) ENGINE=InnoDB AUTO_INCREMENT=70 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `post_comment`
+--
+
+LOCK TABLES `post_comment` WRITE;
+/*!40000 ALTER TABLE `post_comment` DISABLE KEYS */;
+INSERT INTO `post_comment` VALUES (67,2,'Merci !','2021-01-20 07:53:47',80),(68,3,'Le site est cool','2021-01-20 07:54:23',80);
+/*!40000 ALTER TABLE `post_comment` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `post_like`
+--
+
+DROP TABLE IF EXISTS `post_like`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `post_like` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `user` int DEFAULT NULL,
+  `statut` int DEFAULT NULL,
+  `date` datetime DEFAULT NULL,
+  `id_post` int DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  KEY `id_post` (`id_post`),
+  KEY `PostlikeUser` (`user`),
+  CONSTRAINT `post_like_ibfk_1` FOREIGN KEY (`id_post`) REFERENCES `post` (`id`) ON DELETE CASCADE,
+  CONSTRAINT `PostlikeUser` FOREIGN KEY (`user`) REFERENCES `users` (`id`) ON DELETE CASCADE ON UPDATE CASCADE
+) ENGINE=InnoDB AUTO_INCREMENT=56 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `post_like`
+--
+
+LOCK TABLES `post_like` WRITE;
+/*!40000 ALTER TABLE `post_like` DISABLE KEYS */;
+INSERT INTO `post_like` VALUES (49,1,1,'2021-01-20 07:52:03',80),(50,2,1,'2021-01-20 07:52:47',80),(51,3,1,'2021-01-20 07:54:13',80),(52,1,-1,'2021-01-20 07:56:56',81),(53,3,1,'2021-01-20 07:57:02',81);
+/*!40000 ALTER TABLE `post_like` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `users`
+--
+
+DROP TABLE IF EXISTS `users`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `users` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `email` varchar(100) NOT NULL,
+  `password` varchar(255) DEFAULT NULL,
+  `lastname` varchar(40) NOT NULL,
+  `firstname` varchar(40) NOT NULL,
+  `img_profil` varchar(255) DEFAULT NULL,
+  `description` varchar(255) DEFAULT NULL,
+  `role` int DEFAULT (0),
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `email` (`email`)
+) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `users`
+--
+
+LOCK TABLES `users` WRITE;
+/*!40000 ALTER TABLE `users` DISABLE KEYS */;
+INSERT INTO `users` VALUES (1,'jul61@hotmail.fr','$2b$10$70ggN.UIC.2kfSqF/SGOxOuh/dcoysn/aMZJ6CrR14r0/urBdt3Vq','LECHAT','Julien','profile8064-1611015967845.jpg','Développeur Web',1),(2,'test@test.test','$2b$10$J7KfNMKWmKaUIt8DCWg98eJ7ZTTKO6ltMSn6f/iM7PD038RatHKA6','MACRON','Emmanuel','Emmanuel_Macron.jpg',NULL,0),(3,'test2@test.test','$2b$10$6fLcJE5qBu4ogq46p8mJEuzgEUraM0q7Pf1mBy3D2t6krJo9fSWvy','CASTEX','Jean','castex.jpg','Je suis un profil d\'essai',0),(4,'test3@test.test','$2b$10$mBie70fhdS0IDXl.D/UUOenLI6uogWAUamUwFhcxLu5rcFcd1jM1O','Mouloude','Jean',NULL,NULL,0);
+/*!40000 ALTER TABLE `users` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2021-01-20  9:53:11
